5c165ffed023dbca36215b51dc6d0d61ed99aa32
Ryl0308 <97225575+Ryl0308@users.noreply.github.com>
2023-03-17 14:24:53 +0800

    Combined 1 round into a single function
    Switched from using let to var to declare variables

--- script.js
+++ script.js
@@ -17,30 +17,36 @@ function getComputerChoice() {
 
 
 //function takes in players choice and computer's choice, compares the choices and announces the winner
-function playRound(playerSelection, computerSelection) {
-    let outcome
+function playRound() {
+
+    //assigns computer's choice to a variable
+    let computerSelection = getComputerChoice()
+    //to view what the computer's choice is
+    console.log(computerSelection)
+    //assigns player's choice to a variable
+    let playerSelection = (prompt("What's your choice?")).toUpperCase();
 
     if (playerSelection == computerSelection) {
-        outcome = "It's a draw!"
+        var outcome = "It's a draw!"
     }
     //player chooses rock, computer chooses paper
     //player chooses rock, computer chooses scissors 
     else if (playerSelection == "ROCK") {
         if (computerSelection == "PAPER") {
-            outcome = "You lose! Paper beats rock"
+            var outcome = "You lose! Paper beats rock"
         }
         else {
-            outcome = "You win! Rock beats scissors"
+            var outcome = "You win! Rock beats scissors"
         }
     }
     //player chooses paper, computer chooses scissors
     //player chooses paper, computer chooses rock
     else if (playerSelection == "PAPER") {
         if (computerSelection == "SCISSORS") {
-            outcome = "You lose! Scissors beats paper"
+            var outcome = "You lose! Scissors beats paper"
         }
         else {
-            outcome = "You win! Paper beats rock"
+            var outcome = "You win! Paper beats rock"
         }
 
     }
@@ -48,10 +54,10 @@ function playRound(playerSelection, computerSelection) {
     //player chooses scissors, computer chooses paper
     else if (playerSelection == "SCISSORS") {
         if (computerSelection == "ROCK") {
-            outcome = "You lose! Rock beats scissors"
+            var outcome = "You lose! Rock beats scissors"
         }
         else {
-            outcome = "You win! Scissors beats paper"
+            var outcome = "You win! Scissors beats paper"
         }
 
     }
@@ -60,12 +66,11 @@ function playRound(playerSelection, computerSelection) {
     return outcome
 }
 
-//assigns computer's choice to a variable
-let computer = getComputerChoice()
-//to view what the computer's choice is
-console.log(computer)
-//assigns player's choice to a variable
-let playerChoice = prompt("What's your choice?")
+
+
+
+
+
 //announces the winner in the console by passing in the player's and computer's choice into function playround
-console.log(playRound(playerChoice.toUpperCase(), computer))
+console.log(playRound())
 
